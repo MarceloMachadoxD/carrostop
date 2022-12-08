a22b1ebce93da9576ec495975b3df0e8e66a1a6d
MarceloMachadoxD <infomachado1987@gmail.com>
2022-12-08 20:42:41 -0300

    link card

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,26 @@
 import "./styles.css";
+import { Link } from 'react-router-dom';
+
 
 import Car from "../../assets/images/home-car.png";
 function Home() {
   return (
     <>
-      <div className="home-container">
+      <div className="home-banner-container">
         <div className="home-banner">
           <img src={Car} alt="carro vermelho" />
           <h1>O carro perfeito para você</h1>
           <p>
             Conheça nossos carros e dê mais um passo na realização do seu sonho
           </p>
+        </div>
+      </div>
+      <div className="link-card-container" >
+        <div className="link-card">
+            <Link to="products">
+            <button>VER CATALOGO</button>
+            </Link>
+            <p>Comece agora a Navegar</p>
         </div>
       </div>
     </>
