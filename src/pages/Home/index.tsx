076e81557c117331a9e88ad4c6562fb4ef4c5b16
MarceloMachadoxD <infomachado1987@gmail.com>
2022-12-08 19:54:35 -0300

    banner card

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,20 @@
+import "./styles.css";
+
+import Car from "../../assets/images/home-car.png";
 function Home() {
-    return (
-      <>
-      <p>Home</p>
-      </>
-    );
-  }
-  
-  export default Home;
+  return (
+    <>
+      <div className="home-container">
+        <div className="home-banner">
+          <img src={Car} alt="carro vermelho" />
+          <h1>O carro perfeito para você</h1>
+          <p>
+            Conheça nossos carros e dê mais um passo na realização do seu sonho
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
