714ad308976ca2458aa78b3ac7553ec00a8ae485
MarceloMachadoxD <infomachado1987@gmail.com>
2022-12-08 23:04:21 -0300

    Responsividade Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,26 +1,30 @@
 import "./styles.css";
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 import Car from "../../assets/images/home-car.png";
 function Home() {
   return (
     <>
-      <div className="home-banner-container">
-        <div className="home-banner">
-          <img src={Car} alt="carro vermelho" />
-          <h1>O carro perfeito para você</h1>
-          <p>
-            Conheça nossos carros e dê mais um passo na realização do seu sonho
-          </p>
+      <div className="home-container">
+        <div className="home-banner-container">
+          <div className="home-banner">
+            <img src={Car} alt="carro vermelho" />
+            <div className="home-banner-text">
+              <h1>O carro perfeito para você</h1>
+              <p>
+                Conheça nossos carros e dê mais um passo na realização do seu
+                sonho
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="link-card-container" >
-        <div className="link-card">
+        <div className="link-card-container">
+          <div className="link-card">
             <Link to="products">
-            <button>VER CATALOGO</button>
+              <button>VER CATALOGO</button>
             </Link>
             <p>Comece agora a Navegar</p>
+          </div>
         </div>
       </div>
     </>
