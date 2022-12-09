a3e9867445520ac5295bf283f1d9c371afbecf2f
MarceloMachadoxD <infomachado1987@gmail.com>
2022-12-09 09:28:42 -0300

    Componentização SearchBar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,9 +1,14 @@
-import './styles.css'
+import "./styles.css";
 
 const SearchBar = () => {
   return (
     <>
-      <h1>SearchBar</h1>
+      <div className="search-card-container">
+        <div className="link-card">
+          <input type="text" defaultValue="Digite sua busca" />
+          <button>BUSCAR</button>
+        </div>
+      </div>{" "}
     </>
   );
 };
--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -1,24 +1,12 @@
 import "./styles.css";
 import Car from "../../assets/images/catalog-car.png";
+import SearchBar from "../../components/SearchBar/SearchBar";
 
 function Catalog() {
   return (
     <>
       <div className="catalog-container">
-        <div className="search-card-container">
-          <div className="link-card">
-            <input type="text" defaultValue="Digite sua busca" />
-            <button>BUSCAR</button>
-          </div>
-        </div>
-        <div className="link-card-container catalog-card-container">
-          <img src={Car} alt="carro vermelho" />
-          <h1>Audi Supra TT</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, nisi</p>
-          <div className="link-card">
-            <button>COMPRAR</button>
-          </div>
-        </div>
+        <SearchBar />
         <div className="link-card-container catalog-card-container">
           <img src={Car} alt="carro vermelho" />
           <h1>Audi Supra TT</h1>
