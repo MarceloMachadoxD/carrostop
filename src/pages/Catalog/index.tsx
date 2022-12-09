651f68ab41b3a320e5d2274bc98344a15ac75e8d
MarceloMachadoxD <infomachado1987@gmail.com>
2022-12-09 09:55:38 -0300

    product card ajuste de responsividade

--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -7,10 +7,31 @@ function Catalog() {
     <>
       <div className="catalog-container">
         <SearchBar />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        <div className="container my-4">
+          <div className="row">
+            <div className="col-sm-6 col-lg-4 col-xl-4">
+              <ProductCard />
+            </div>
+            <div className="col-sm-6 col-lg-4 col-xl-4">
+              <ProductCard />
+            </div>
+            <div className="col-sm-6 col-lg-4 col-xl-4">
+              <ProductCard />
+            </div>
+            <div className="col-sm-6 col-lg-4 col-xl-4">
+              <ProductCard />
+            </div>
+            <div className="col-sm-6 col-lg-4 col-xl-4">
+              <ProductCard />
+            </div>
+            <div className="col-sm-6 col-lg-4 col-xl-4">
+              <ProductCard />
+            </div>
+            <div className="col-sm-6 col-lg-4 col-xl-4">
+              <ProductCard />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
